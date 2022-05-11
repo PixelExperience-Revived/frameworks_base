6606a8e0639358132887e3c7ae65051e52725b3f
Ganesh Olekar <olekarg@google.com>
2022-05-11 22:15:21 +0000

    DO NOT MERGE Fix auto-grant of AR runtime permission if device is upgrading from pre-Q
    Test: Manually install app apks targeting Q and verifying that AR permission is not auto-granted
    Test: atest ActivityRecognitionPermissionTest
    Bug: 210065877
    Change-Id: I90adf45a6611ab8bc953765c72af77a6a4f7aae8
    (cherry picked from commit 84b1160cfc382f86af57f50d27864276d6c93f4a)
    (cherry picked from commit 1c4e2160bcb089adce0bc36a7a7badd5e5a87702)
    Merged-In: I90adf45a6611ab8bc953765c72af77a6a4f7aae8

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -25354,6 +25354,9 @@ public class PackageManagerService extends IPackageManager.Stub
         public void userRemovedForTest() {
             mBlockDeleteOnUserRemoveForTest.open();
         }
+        public int getInstalledSdkVersion(PackageParser.Package pkg) {
+            return PackageManagerService.this.getSettingsVersionForPackage(pkg).sdkVersion;
+        }
     }
 
     @GuardedBy("mPackages")
